2149b3a11ba52e456c13f1e91f807ad2d7eb7847
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-07-25 12:46:36 +0100

    Complete footer

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,15 @@
 const burgerBtn = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.nav__menu--mobile');
 const mobileLinks = mobileMenu.querySelectorAll('.nav__menu--mobile a');
+const footerYear = document.querySelector('.footer__year');
 
 const handleMenu = () => {
 	burgerBtn.classList.toggle('is-active');
 	mobileMenu.classList.toggle('nav__menu--mobile--active');
 	document.body.classList.toggle('turnOffScroll');
 };
+
+footerYear.textContent = new Date().getFullYear();
 
 mobileLinks.forEach((link) => {
 	link.addEventListener('click', handleMenu);
